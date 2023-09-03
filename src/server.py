84f68158833532022a91085dfddbf5c55df9d80e
felse <github@xlfe.de>
2023-09-03 16:55:30 +0200

    refactor wfile.write

--- src/server.py
+++ src/server.py
@@ -20,22 +20,22 @@ class MyServer(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header("Content-type", CONTENT_TYPE)
             self.end_headers()
-            self.wfile.write(bytes("<html><head><title>fettnapf 3000 recipes</title></head>", "utf-8"))
-            self.wfile.write(bytes("<body>", "utf-8"))
-            self.wfile.write(bytes(self.path + "is no valid path!", "utf-8"))
-            self.wfile.write(bytes("</body></html>", "utf-8"))
+            self.wfile_write("<html><head><title>fettnapf 3000 recipes</title></head>")
+            self.wfile_write("<body>")
+            self.wfile_write(self.path + "is no valid path!")
+            self.wfile_write("</body></html>")
 
     def get_recipes(self):
         self.send_response(200)
         self.send_header("Content-type", CONTENT_TYPE)
         self.end_headers()
-        self.wfile.write(bytes("<html><head><title>fettnapf 3000 recipes</title></head>", "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
+        self.wfile_write("<html><head><title>fettnapf 3000 recipes</title></head>")
+        self.wfile_write("<body>")
         button = "Stelle Menge pro Gericht ein und drueck auf Kalkulation!"
         button += "<button onclick=\"window.location.href='calculate?recipe=apfelkrapfen.txt&quantity=1000';\">Kalkulation</button>"
-        self.wfile.write(bytes(button, "utf-8"))
-        self.wfile.write(bytes(self.create_recipes_table(), "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
+        self.wfile_write(button)
+        self.wfile_write(self.create_recipes_table())
+        self.wfile_write("</body></html>")
 
     def create_recipes_table(self):
         html_string = "<table>"
@@ -54,22 +54,22 @@ class MyServer(BaseHTTPRequestHandler):
         params = urllib.parse.parse_qs(self.path[self.path.rfind("?") + 1:])
         recipe_name = params.get("recipe").pop()
         quantity = int(params.get("quantity").pop())
-        # self.wfile.write(bytes("recipe: " + recipe_name + "<br>", "utf-8"))
-        # self.wfile.write(bytes("quantity: " + str(quantity) + "<br>", "utf-8"))
         recipe = parser.parse_recipe("recipes/" + recipe_name)
         menu = {"recipe": [(recipe, quantity)]}
         
         plan = planner.plan(menu)
         plan_html = markdown2.markdown(plan, extras=['tables','task_list'])
         
-        self.wfile.write(bytes("<html><head><title>"
+        self.wfile_write("<html><head><title>"
                                + str(quantity)
                                + " " + recipe_name
-                               + "</title></head>", "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes(plan_html, "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
+                               + "</title></head>")
+        self.wfile_write("<body>")
+        self.wfile_write(plan_html)
+        self.wfile_write("</body></html>")
 
+    def wfile_write(self, string):
+        self.wfile.write(bytes(string, "utf-8"))
 
 os.chdir('.')
 if len(sys.argv) == 2:
