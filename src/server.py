aa98b9ed2f549edbe66ba04f8606d9fb85893b60
ovi <ovi@posteo.de>
2023-09-06 12:14:56 +0200

    replacing old server

--- src/server.py
+++ src/server.py
@@ -1,116 +1,113 @@
-import os
 import sys
-import urllib.parse
-import parser
+import os, os.path
+import cherrypy
+import markdown
+import pymdownx
 import planner
-from http.server import HTTPServer, BaseHTTPRequestHandler
-import markdown2
+import parser
 
-DEFAULT_ADDR = ''
-DEFAULT_PORT = 8080
-CONTENT_TYPE = "text/html; charset=utf-8"
-
-
-class MyServer(BaseHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == "/recipes":
-            self.get_recipes()
-        elif self.path.startswith("/calculate"):
-            self.get_calculate()
-        else:
-            self.send_response(200)
-            self.send_header("Content-type", CONTENT_TYPE)
-            self.end_headers()
-            self.wfile_write("<html><head><title>fettnapf 3000 recipes</title></head>")
-            self.wfile_write("<body>")
-            self.wfile_write(self.path + "is no valid path!")
-            self.wfile_write("</body></html>")
-
-    def get_recipes(self):
-        self.send_response(200)
-        self.send_header("Content-type", CONTENT_TYPE)
-        self.end_headers()
-        self.wfile_write(
-            "<html>\
-            <head>\
-            <title>fettnapf 3000 recipes</title>\
-            <style>\
-            form  { display: table;      }\
-            p     { display: table-row;  }\
-            label { display: table-cell; }\
-            input { display: table-cell; }\
-            </style>\
-            </head>")
-        self.wfile_write("<body>")
-        hint = "<strong>Stelle Anzahl Portionen pro Gericht ein und drueck auf Kalkulation!</strong>"  \
-        "<br> Speicher danach den Link, um deine Kalkulation zu teilen.<br><br>"
-        self.wfile_write(hint)
-        self.wfile_write(self.create_recipes_form())
-        self.wfile_write("</body></html>")
+class RecipePage:
+    @cherrypy.expose
+    def index(self):
+        return f"""<html>
+          <head>
+            <link href="./static/css/recipes.css" rel="stylesheet">
+            <link rel="icon"
+                  href="data:image/svg+xml,
+                        <svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22>
+                         <text y=%22.9em%22 font-size=%2290%22>&#127814;</text>
+                        </svg>"
+            >
+            <title>fettnapf3000 Power Kalkulator!</title>
+          </head>
+          <body>
+           <center><p style="font-size:70px;">
+            &#127814;
+           </p></center>
+           <strong>Stelle Anzahl Portionen pro Gericht ein und drück auf Kalkulation!</strong>
+           <br> Speicher danach den Link, um deine Kalkulation zu teilen.<br><br>
+           {self.create_recipes_form()}
+         </body>
+         <center>
+         <footer>
+           <p>made with &#127814; by team geil</p>
+           <p>contribute on <a href="https://github.com/ooovi/fettnapf3000">github</a></p>
+         </footer>
+         </center>
+         </html>"""
 
     def create_recipes_form(self):
-        recipes = os.listdir("recipes")
+        recipes = os.listdir("../recipes")
+        recipes.sort()
         html_string = " <form action=\"/calculate\" method=\"get\">"
         for recipe in recipes:
-            html_string += "<p>\
-                <label for=\"" + recipe + "\">" + os.path.splitext(recipe)[0].capitalize().replace("_"," ") + ":&ensp;</label>\
-                <input type=\"number\" name=\"" + recipe + "\" size=\"6\"><br>\
-                </p>"
+            html_string += f"""<p>
+                <label for="{recipe}">
+                 {os.path.splitext(recipe)[0].capitalize().replace("_"," ")}:&ensp;
+                </label>
+                <input type="number" name="{recipe}" size="6"><br>
+                </p>"""
         html_string += "<input type=\"submit\" value=\"Kalkulation\"></form>"
         return html_string
 
-    def get_calculate(self):
-        self.send_response(200)
-        self.send_header("Content-type", CONTENT_TYPE)
-        self.end_headers()
-        params = urllib.parse.parse_qs(self.path[self.path.rfind("?") + 1:])
-
+class CalculatePage:
+    @cherrypy.expose
+    def index(self, **kwargs):
         menu = {}
-        for recipe_name in params:
-            recipe = parser.parse_recipe("recipes/" + recipe_name)
-            n_servings = int(params.get(recipe_name).pop())
-            if "Rezepte" in menu:
-                 menu["Rezepte"].append((recipe, n_servings))
-            else:
-                 menu["Rezepte"] = [(recipe, n_servings)]
+        for (recipe_name, n) in kwargs.items():
+            if n:
+                recipe = parser.parse_recipe("../recipes/" + recipe_name)
+                n_servings = int(n)
+                if "Rezepte" in menu:
+                     menu["Rezepte"].append((recipe, n_servings))
+                else:
+                     menu["Rezepte"] = [(recipe, n_servings)]
             
         plan = planner.plan(menu)
-        plan_html = markdown2.markdown(plan, extras=['tables','task_list'])
-        
-        self.wfile_write("<html>\
-            <head>\
-            <style>\
-            body { \
-            	max-width: 1080px; \
-            	margin: 10 auto !important; \
-            	float: none !important; \
-            }\
-            h1 {border-top: 2px solid lightgrey; padding-top:50px}\
-            table {\
-               border-collapse: collapse;\
-               margin: 25px 0;\
-               min-width: 400px;\
-            }\
-            table tr:nth-child(even) td { background-color: #f2f2f2;}\
-            table td { padding: 10px; }\
-            </style>\
-            <title>Fettnapf3000 Power Kalkulator!</title></head>")
-        self.wfile_write("<body>")
-        self.wfile_write(plan_html)
-        self.wfile_write("</body></html>")
 
-    def wfile_write(self, string):
-        self.wfile.write(bytes(string, "utf-8"))
+        extension_configs = { 'pymdownx.tasklist': {'clickable_checkbox': 'True' } }
+        plan_html = markdown.markdown(plan,
+            extensions=['tables','pymdownx.tasklist'],
+            extension_configs=extension_configs)
 
-os.chdir('.')
-if len(sys.argv) == 3:
-    addr = sys.argv[1]
-    port = sys.argv[2]
-if len(sys.argv) == 2:
-    addr = DEFAULT_ADDR
-    port = sys.argv[1]
-else:
-    addr = DEFAULT_ADDR
-    port = DEFAULT_PORT
-server_object = HTTPServer(server_address=(addr, int(port)), RequestHandlerClass=MyServer)
-server_object.serve_forever()
+        return f"""<html>
+             <head>
+              <link href="../static/css/calculate.css" rel="stylesheet">
+              <link rel="icon"
+                    href="data:image/svg+xml,
+                          <svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22>
+                           <text y=%22.9em%22 font-size=%2290%22>&#127814;</text>
+                          </svg>"
+              >
+              <title>fettnapf3000 Power Kalkulator!</title>
+             </head>
+           <center><p style="font-size:70px;">
+            <a href="/">&#127814;</a>
+           </p></center>
+             <body>
+              {plan_html}
+             </body>
+            </html>"""
+
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 3:
+        cherrypy.config.update({'server.socket_host': sys.argv[1],
+                                'server.socket_port': int(sys.argv[2]),
+                               })
+    if len(sys.argv) == 2:
+        cherrypy.config.update({'server.socket_port': int(sys.argv[1])})
+    conf = {
+        '/': {
+            'tools.sessions.on': True,
+            'tools.staticdir.root': os.path.abspath(os.getcwd())
+        },
+        '/static': {
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': './public'
+        }
+    }
+    root = RecipePage()
+    root.calculate = CalculatePage()
+    cherrypy.quickstart(root, config = conf)
