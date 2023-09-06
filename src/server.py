70ca68ea6eac3f285205c9df29d5689cf72c8316
ovi <ovi@posteo.de>
2023-09-06 15:33:23 +0200

    mobile prettyness

--- src/server.py
+++ src/server.py
@@ -18,35 +18,43 @@ def randomoji():
                    "&#129473;",
                    "&#127789;"])
 
-class RecipePage:
-    @cherrypy.expose
-    def index(self):
-        return f"""<html>
-          <head>
-            <link href="./static/css/recipes.css" rel="stylesheet">
-            <link rel="icon"
+def favicon():
+   return """<link rel="icon"
                   href="data:image/svg+xml,
                         <svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22>
                          <text y=%22.9em%22 font-size=%2290%22>&#127814;</text>
                         </svg>"
-            >
-            <title>fettnapf3000 Power Kalkulator!</title>
-          </head>
-          <body>
-           <center><p style="font-size:70px;">
-            {randomoji()}
-           </p></center>
-           <strong>Stelle Anzahl Portionen pro Gericht ein und drück auf Kalkulation!</strong>
-           <br> Speicher danach den Link, um deine Kalkulation zu teilen.<br><br>
-           {self.create_recipes_form()}
-         </body>
-         <center>
-         <footer>
-           <p>made with &#127814; by team geil</p>
-           <p>contribute on <a href="https://github.com/ooovi/fettnapf3000">github</a></p>
-         </footer>
-         </center>
-         </html>"""
+              >"""
+
+def viewport():
+    return """<meta name="viewport" content="width=device-width, initial-scale=1.0">"""
+
+
+class RecipePage:
+    @cherrypy.expose
+    def index(self):
+        return f"""<html>
+              {viewport()}
+              <head>
+               <link href="./static/css/recipes.css" rel="stylesheet">
+               {favicon()}
+               <title>fettnapf3000 Power Kalkulator!</title>
+             </head>
+             <body>
+              <center><p style="font-size:70px;">
+               {randomoji()}
+              </p></center>
+              <strong>Stelle Anzahl Portionen pro Gericht ein und drück auf Kalkulation!</strong>
+              <br> Speicher danach den Link, um deine Kalkulation zu teilen.<br><br>
+              {self.create_recipes_form()}
+            </body>
+            <center>
+            <footer>
+              <p>made with &#127814; by team geil</p>
+              <p>contribute on <a href="https://github.com/ooovi/fettnapf3000">github</a></p>
+            </footer>
+            </center>
+            </html>"""
 
     def create_recipes_form(self):
         recipes = os.listdir("../recipes")
@@ -90,25 +98,21 @@ class CalculatePage:
             extension_configs=extension_configs)
 
         return f"""<html>
-             <head>
-              <link href="../static/css/calculate.css" rel="stylesheet">
-              <link rel="icon"
-                    href="data:image/svg+xml,
-                          <svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22>
-                           <text y=%22.9em%22 font-size=%2290%22>&#127814;</text>
-                          </svg>"
-              >
-              <title>fettnapf3000 Power Kalkulator!</title>
-             </head>
-              <body>
-               <center><p style="font-size:70px;">
-                <a href="/">
+            {viewport()}
+            <head>
+             <link href="../static/css/calculate.css" rel="stylesheet">
+             {favicon()}
+             <title>fettnapf3000 Power Kalkulator!</title>
+            </head>
+             <body>
+              <center><p style="font-size:70px;">
+               <a href="/">
                 {randomoji()}
-                </a>
-               </p></center>
-               {plan_html}
-              </body>
-            </html>"""
+               </a>
+              </p></center>
+              {plan_html}
+             </body>
+           </html>"""
 
 if __name__ == '__main__':
     if len(sys.argv) == 3:
